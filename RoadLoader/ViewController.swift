3601d62796835b98f9ff64f917bd0fb7c2004abf
wataru0 <wataru.tv.nanao@gmail.com>
2020-10-17 12:26:50 +0900

    initial commit

---
+++ RoadLoader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RoadLoader
+//
+//  Created by 岡本航昇 on 2020/10/17.
+//  Copyright © 2020 wataru okamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
