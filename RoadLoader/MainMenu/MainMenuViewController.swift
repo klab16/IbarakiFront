8d942cad0a721981064bf76a6ab42e8252b3212b
wataru0 <wataru.tv.nanao@gmail.com>
2020-10-21 21:49:37 +0900

    Iconの追加

---
+++ RoadLoader/MainMenu/MainMenuViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MainMenuViewController.swift
+//  RoadLoader
+//
+//  Created by 岡本航昇 on 2020/10/21.
+//  Copyright © 2020 wataru okamoto. All rights reserved.
+//
+
+import Foundation
---
+++ RoadLoader/ObjectDetection2/DrawingBoundingBoxView.swift
@@ -0,0 +1,9 @@
+//
+//  DrawingBoundingBoxView.swift
+//  RoadLoader
+//
+//  Created by 岡本航昇 on 2020/10/20.
+//  Copyright © 2020 wataru okamoto. All rights reserved.
+//
+
+import Foundation
